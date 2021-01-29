ed433af2f681d40868383fa377b7db2a9079b9a5
ChebnevDev <chebnevdev@gmail.com>
2021-01-29 22:01:21 +0300

    Initial commit

---
+++ app/src/main/java/com/chebnevdev/mobiledevelopmentcourse/TasksMainActivity.kt
@@ -0,0 +1,15 @@
+package com.chebnevdev.mobiledevelopmentcourse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.tasks_main_activity.*
+
+class TasksMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tasks_main_activity)
+
+
+    }
+}
